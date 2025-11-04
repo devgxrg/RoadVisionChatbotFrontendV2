7d5ea26c3746ddb5c5d0033d79141a67216d4db8
WinterSunset95 <wintersunset95@gmail.com>
2025-11-04 11:25:48 +0530

    fix: using local server for now

--- src/lib/config/api.ts
+++ src/lib/config/api.ts
@@ -1,5 +1,6 @@
 const isProduction = window.location.protocol === 'https:';
 
-export const API_BASE_URL = isProduction
-  ? 'https://aware-thoroughly-goldfish.ngrok-free.app/api/v1'
-  : '/api';
+// export const API_BASE_URL = isProduction
+//   ? 'https://aware-thoroughly-goldfish.ngrok-free.app/api/v1'
+//   : '/api';
+export const API_BASE_URL = 'https://aware-thoroughly-goldfish.ngrok-free.app/api/v1';
